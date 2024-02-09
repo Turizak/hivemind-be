a2edade0f5f09a47747103460680cb9ce25dc1de
Robert Kazirut <robert.kazirut@gmail.com>
2024-02-09 18:24:52 -0500

    add cors package use default settings

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"example/hivemind-be/routes"
 	"example/hivemind-be/db"
+	"example/hivemind-be/routes"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	db.ConnectDatabase()
+	router.Use(cors.Default())
 	routes.Routes(router)
 	router.Run(":8080")
 }
