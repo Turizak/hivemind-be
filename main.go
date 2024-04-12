2e4d9c7bc11af878db08b433aff1a50c10e7d7d3
Robert Kazirut <robert.kazirut@gmail.com>
2024-04-12 14:32:45 -0400

    allow auth header

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"example/hivemind-be/db"
 	"example/hivemind-be/routes"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,13 @@ import (
 func main() {
 	router := gin.Default()
 	db.ConnectDatabase()
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Change this to the origin you want to allow
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	routes.Routes(router)
 	router.Run(":8080")
 }
