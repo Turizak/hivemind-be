dde1fc0fae9df1748bcc2dbf852801b59c0c2ca0
Robert Kazirut <robert.kazirut@gmail.com>
2024-08-14 12:37:12 -0400

    change cors middlewear (test)

--- main.go
+++ main.go
@@ -13,11 +13,10 @@ func main() {
 	router := gin.Default()
 	db.ConnectDatabase()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"}, // Change this to the origin you want to allow
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowOrigins: []string{"*"}, // Change this to the origin you want to allow
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		MaxAge:       12 * time.Hour,
 	}))
 	routes.Routes(router)
 	router.Run(":8080")
